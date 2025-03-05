8cc6cb8dc68ba94a26d69f28eacf598145ba79c0
Grant Hawkins <granthawkins88@gmail.com>
2025-03-05 13:18:14 +0700

    debug statements

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,9 +1,11 @@
 const origin = () => {
+  console.log(window.location.origin);
   return window.location.origin.split(":").slice(0, 2).join(":");
 };
 
 const env = () => {
   const _origin = origin();
+  console.log(_origin);
   if (
     _origin.includes("localhost") ||
     _origin.includes("12") ||
